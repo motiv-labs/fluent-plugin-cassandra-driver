23ccd90ba934df6b8064f21968dcdc3ad3485cbb
Aristotle <11413392+tuxranger@users.noreply.github.com>
2021-03-27 14:50:27 -0400

    [skip ci] added logic to skip ci

--- .circleci/update-impulse.sh
+++ .circleci/update-impulse.sh
@@ -4,6 +4,7 @@
 # $1 is the name of the micro service
 # $2 is the image id
 # $3 is the GITHUB OAUTH Key
+# $4 is the commit message
 # This script will first get the latest raw of the docker-compose file for impulse and copy it into a local file
 # It will then replace the entire name of the image for the passed in ($1) micro service with the image name that is built
 # Finally it will commit the new docker-compose file to impulse with a message
@@ -18,7 +19,11 @@ curl -H 'Authorization: token '"$3"'' -H 'Accept: application/vnd.github.v3.raw'
 sed -i "s+motivlabs/$1:.*+$2+g" astro.conf
 # echo pushing file to GITHUB
 
-curl -X PUT -H 'Authorization: token '"$3"'' -d '{"message":"updated fluentd-impulse image","content":"'"$(base64 -w0 astro.conf)"'","sha":'"$(curl -s -X GET -H 'Authorization: token '"$3"'' https://api.github.com/repos/motiv-labs/impulse/contents/docker-tools/compose-template/config/astro.conf | jq .sha)"'}' -L https://api.github.com/repos/motiv-labs/impulse/contents/docker-tools/compose-template/config/astro.conf
+if [[ $(echo "$4" | grep -c "[skip ci]\|[ci skip]") == 1 ]]; then
+  curl -X PUT -H 'Authorization: token '"$3"'' -d '{"message":"[skip ci] updated fluentd-impulse image","content":"'"$(base64 -w0 astro.conf)"'","sha":'"$(curl -s -X GET -H 'Authorization: token '"$3"'' https://api.github.com/repos/motiv-labs/impulse/contents/docker-tools/compose-template/config/astro.conf | jq .sha)"'}' -L https://api.github.com/repos/motiv-labs/impulse/contents/docker-tools/compose-template/config/astro.conf
+else
+  curl -X PUT -H 'Authorization: token '"$3"'' -d '{"message":"updated fluentd-impulse image","content":"'"$(base64 -w0 astro.conf)"'","sha":'"$(curl -s -X GET -H 'Authorization: token '"$3"'' https://api.github.com/repos/motiv-labs/impulse/contents/docker-tools/compose-template/config/astro.conf | jq .sha)"'}' -L https://api.github.com/repos/motiv-labs/impulse/contents/docker-tools/compose-template/config/astro.conf
+fi
 
 # update googlecloud image
 #curl -H 'Authorization: token '"$3"'' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/motiv-labs/impulse-googlecloud/contents/impulse/taxi-dropoff-dotcms-app-deployment.yaml
